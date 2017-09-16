8de9c3fc1b195135947820bf38f6882b55fbe2ab
Prabhu Konchada <prabhukonchada@Prabhus-MacBook-Pro.local>
2017-09-16 14:34:20 +0530

    Used toylist to append to a text view

--- app/src/main/java/com/example/android/favouritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favouritetoys/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.android.favouritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView) findViewById(R.id.toyTextView);
+        String[] toyNames = ToyBox.getToyNames();
+
+        for (String toyName : toyNames)
+        {
+            mToysListTextView.append(toyName+"\n\n\n");
+        }
+
     }
 }
 
